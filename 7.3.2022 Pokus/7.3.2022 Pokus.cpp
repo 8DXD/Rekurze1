7efc985dbf11b6568431f8e3a2bcde17a24e0ad8
8DXD <kacule05@gmail.com>
2022-03-07 14:11:14 +0100

    Úroky

--- 7.3.2022 Pokus/7.3.2022 Pokus.cpp
+++ 7.3.2022 Pokus/7.3.2022 Pokus.cpp
@@ -1,4 +1,5 @@
 ﻿#include <stdio.h>
+#include <math.h>
 
 //Funkce faktoriál
 long faktorial(int x) {
@@ -10,20 +11,57 @@ long faktorial(int x) {
 	}
 }
 
-//Pomocí rekuez funkcí, vypočtěte počet buněk po N dnech, když za každý den se počet buněk zdvojnásobí (na začátku je jedna)
+//Rekurzi lze obvykle linearizovat
+
+//Pomocí rekuezi funkcí, vypočtěte počet buněk po N dnech, když za každý den se počet buněk zdvojnásobí (na začátku je jedna)
 long bunky(int x) {
 	if (x <= 1) {
 		return 1;
 	}
 	else {
-		return 2 * faktorial(x - 1);
+		return 2 * bunky(x - 1);
 	}
 }
 
+//Fibonacciho posloupnost
+long fibonacci(int x) {
+	if (x < 2) {
+		if (x < 1) {
+			return 0;
+		}
+		else {
+			return 1;
+		}
+	}
+	else {
+		return fibonacci(x - 1) + fibonacci(x - 2);
+	}
+}
+
+//Úročení po dnech (složený úrok)
+long urok(int x) {
+	int a = 10000;
+	if (x < 1) {
+			return 0;
+	}
+	else {
+		return a*1,12;
+	}
+
+}
+
+
 int main()
 {
-	printf("1. %d\n", faktorial(1));
-	printf("2. %d\n", faktorial(2));
-	printf("3. %d\n", faktorial(3));
-	printf("4. %d\n", faktorial(4));
+	printf("Fibonacciho posloupnost\n");
+	for (int i = 0; i < 6; i++) {
+		printf("%2d. %2d\n", i, fibonacci(i));
+	}
+
+	printf("\n");
+
+	printf("Slozene uroceni - 10 000Kc - urok 12% rocne\n");
+	for (int j = 0; j < 6; j++) {
+		printf("%2d.rok %2d\n", j, fibonacci(j));
+	}
 }
