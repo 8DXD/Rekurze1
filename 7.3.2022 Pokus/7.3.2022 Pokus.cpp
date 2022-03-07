7a262334a9cea04c573bed0c3c54fc5a818e5472
8DXD <kacule05@gmail.com>
2022-03-07 13:27:40 +0100

    Přidejte soubory projektu.

---
+++ 7.3.2022 Pokus/7.3.2022 Pokus.cpp
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+
+//Funkce faktoriál
+long faktorial(int x) {
+	if (x <= 1) {
+		return 1;
+	}
+	else {
+		return x * faktorial(x - 1);
+	}
+}
+
+
+
+int main()
+{
+	printf("1. %d\n", faktorial(1));
+	printf("2. %d\n", faktorial(2));
+	printf("3. %d\n", faktorial(3));
+	printf("4. %d\n", faktorial(4));
+}
